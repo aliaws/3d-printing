eaf08a24f00d9cbeb8f9a5d99a1bcaeb1395f3d3
Ali Abbas <ali@accuratewebsolution.com>
2024-01-18 21:37:31 +0500

    minor fixes of the plugin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function ($) {
-  console.log('js loaded')
   jQuery('#add_field').on('click', function () {
     const input_field = '<p><input step="0.01" name="layer_heights[]" class="regular-text" type="number" ><span title="Remove field" class="remove_layer_height_btn">x</span></p>';
     jQuery('.nozzle_diameter_wrapper').append(input_field);
@@ -8,6 +7,4 @@ jQuery(document).ready(function ($) {
 
     jQuery(this).parents("p").remove();
   })
-
-  // Handle form submission
 });
--- frontend/add_to_cart_response.php
+++ frontend/add_to_cart_response.php
@@ -1,3 +1,4 @@
-<p>The request to print your file <strong><?php echo $file_name; ?></strong> has been added to cart
+<p class="file-input-container">
+  <span>The request to print your file <strong><?php echo $file_name; ?></strong> has been added to cart. </span>
   <a class="button wp-element-button" href="<?php echo wc_get_cart_url(); ?>" target="_self">View Cart</a>
 </p>
