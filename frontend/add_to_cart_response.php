f88aa0e1c4494309e146bf15699cf8f55e2672a0
Ali Abbas <ali@accuratewebsolution.com>
2024-01-18 16:57:58 +0500

    added view cart button

--- frontend/add_to_cart_response.php
+++ frontend/add_to_cart_response.php
@@ -1,1 +1,3 @@
-<p>The request to print your file <strong><?php echo $file_name; ?></strong> has been added to cart</p>
+<p>The request to print your file <strong><?php echo $file_name; ?></strong> has been added to cart
+  <a class="button wp-element-button" href="<?php echo wc_get_cart_url(); ?>" target="_self">View Cart</a>
+</p>
