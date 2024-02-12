c181b1646906a044e730fc62212eec8cae09bcdb
Ali Abbas <itsgeniusstar@gmail.com>
2024-02-12 14:14:09 +0500

    Same plugin that deployed on rprints site

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-jQuery('#add_field').on('click', function () {
-    const input_field = '<p><input name="layer_heights[]" class="regular-text" type="number" ><span title="Remove field" class="remove_layer_height_btn">x</span></p>';
-    jQuery('.nozzle_diameter_wrapper').append(input_field);
-});
-jQuery(document).on('click', '.remove_layer_height_btn', function () {
-
-    jQuery(this).parents("p").remove();
-})
